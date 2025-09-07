a41d3461e295d728fce450df24555113adf2f510
LokithanGR <148251021+LokithanGR@users.noreply.github.com>
2025-09-07 23:31:26 +0530

    Delete src directory

--- src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api' });
-api.interceptors.request.use(cfg => {
-  const token = localStorage.getItem('token');
-  if (token) cfg.headers.Authorization = `Bearer ${token}`;
-  return cfg;
-});
-export default api;
